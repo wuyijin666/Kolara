04291d679e0c387d6405cf593eeb4bd2b531c8b8
吴静文 <935301185@qq.com>
2025-08-03 10:32:22 +0800

    Echo Server (回显服务器)，帮助理解socket网络编程的基本流程，掌握go程对多连接并发的处理

---
+++ myDemo/KolaraV0.1/Client.go
@@ -0,0 +1,38 @@
+package main
+
+import(
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	fmt.Println("client is starting...")
+	// 连接远程服务器
+	conn, err := net.Dial("tcp" , "127.0.0.1:8999")
+	if err != nil {
+		fmt.Println("client start is err :" , err)
+		return
+	}
+
+	for {
+		// 连接调用write 写数据
+		_, err := conn.Write([]byte("hello im KolaraV0.1 !"))
+		if err != nil {
+			fmt.Println("write to server err: ", err)
+			return
+		}
+
+		buf := make([]byte, 512)
+	    cnt, err :=	conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn read is err :" , err)
+			return 
+		}
+		fmt.Printf("server call back : %s, cnt = %d\n", buf[:cnt], cnt)
+
+		// cpu阻塞， 目的是不要一直for循环
+		time.Sleep(1*time.Second)
+
+	}
+}
