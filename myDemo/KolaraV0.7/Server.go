ffc66925e34f2016496d1c61e31a0e468c0297f7
吴静文 <935301185@qq.com>
2025-08-08 17:17:56 +0800

    读写协程分离模型集成Kolara框架

---
+++ myDemo/KolaraV0.7/Server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"Kolara/knet"
+	"Kolara/kiface"
+	"fmt"
+)
+
+// 利用Kolara框架的api, 开发服务器端应用程序
+
+// ping test 自定义路由
+type PingRouter struct {
+	knet.BaseRouter
+}
+
+type HelloKolaraRouter struct {
+	knet.BaseRouter
+}
+
+func (p *PingRouter) Handle(request kiface.IRequest) {
+	fmt.Println("Call PingRouter Handle")
+	// 先读取客户端数据 再回写ping ping ping
+	fmt.Println("recv from client msgId:", request.GetMsgId(), "data:", string(request.GetData()))
+
+    err := request.GetConnection().SendMsg(200, []byte("ping ping ping\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (h *HelloKolaraRouter) Handle(request kiface.IRequest) {
+	fmt.Println("Call HelloKolaraRouter Handle")
+	// 先读取客户端数据 再回写ping ping ping
+	fmt.Println("recv from client msgId:", request.GetMsgId(), "data:", string(request.GetData()))
+
+	err := request.GetConnection().SendMsg(201, []byte("Hello welcome to KolaraV0.7!\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+
+
+func runServer() {
+	// 1. 创建一个server句柄，利用Kolara框架的api
+	s := knet.NewServer("Kolara V0.7")
+
+	// 2. 给当前框架添加自定义的router
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HelloKolaraRouter{})
+
+	// 3. 启动服务器
+	s.Serve()
+}
+
+
+// func main() {
+// 	runServer()
+// }
